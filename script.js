88d5898c278427fe1feb62a51876e076af81b16c
ahfercen <ahfercen@gmail.com>
2022-10-11 21:33:08 -0400

    JS 16x16 div generation

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+console.log(container);
+
+for(i = 0; i<16;i++){
+    let row = document.createElement("div");
+    row.classList.add(`row`);
+    row.classList.add(`row${i}`);
+    for(j=0;j<16;j++){
+        let col = document.createElement("div");
+        col.classList.add("col");
+        col.classList.add(`col${j}`);
+        row.appendChild(col);
+    }
+    container.appendChild(row);
+}
