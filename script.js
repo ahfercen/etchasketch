c2d9526c3447ce52afecd666de197a336e011b46
ahfercen <ahfercen@gmail.com>
2022-10-12 17:50:03 -0400

    function to reset board to custom size

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 const container = document.querySelector(".container");
+const resetButton = document.querySelector(".Controls button");
+function resetFunc(x,y){
+    let board = document.querySelector('.board');
+    container.removeChild(board);
+    createBoard(x,y);
+}
+resetButton.addEventListener('click', function(){
+    let x = prompt("Choose a X value");
+    let y = prompt("Choose a Y value");
+    resetFunc(x,y);
+});
 function hover(e){
     e.target.classList.add('hover');
 }
@@ -6,18 +17,26 @@ function goBack(e){
     if(e.propertyName !== 'background-color') return;
     e.target.classList.remove('hover');
 }
-for(i = 0; i<16;i++){
-    let row = document.createElement("div");
-    row.classList.add(`row`);
-    row.classList.add(`row${i}`);
-    for(j=0;j<16;j++){
-        let col = document.createElement("div");
-        col.classList.add("col");
-        col.classList.add(`col${j}`);
-        row.appendChild(col);
-        col.addEventListener('mouseover',hover);
-        col.addEventListener('transitionend',goBack);
+function createBoard(x,y){
+    let board = document.createElement("div");
+    board.className = "board";
+
+    for(i = 0; i<y;i++){
+        let row = document.createElement("div");
+        row.classList.add(`row`);
+        row.classList.add(`row${i}`);
+        for(j=0;j<x;j++){
+            let col = document.createElement("div");
+            col.classList.add("col");
+            col.classList.add(`col${j}`);
+            row.appendChild(col);
+            col.addEventListener('mouseover',hover);
+            col.addEventListener('transitionend',goBack);
+        }
+        board.appendChild(row);
+        container.appendChild(board);
     }
-    container.appendChild(row);
 }
 
+//Starter function with 16x16 board
+createBoard(16,16);
